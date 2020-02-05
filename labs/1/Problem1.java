7519bdf907f729008f070ffe4750c7fb4845949d
jschinasi <jake.schinasi1@marist.edu>
2020-02-05 18:28:10 -0500

    Problem 1.java

---
+++ labs/1/Problem1.java
@@ -0,0 +1,16 @@
+public class Problem1 {
+
+    public static void main(final String[] args) {
+
+        final double n;
+        final double d;
+
+        n = (15.4*6.5-4.3*3);
+        d = (46.3-3.5);
+
+        System.out.println(n/d);
+    
+
+    }
+
+}
