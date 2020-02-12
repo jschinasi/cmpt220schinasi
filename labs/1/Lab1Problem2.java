e4d26ce6f2f99f3d32d4efe1a613f60b051ee04b
jschinasi <jake.schinasi1@marist.edu>
2020-02-12 18:57:27 -0500

    Lab1Problem2 renamed

---
+++ labs/1/Lab1Problem2.java
@@ -0,0 +1,18 @@
+public class Problem2 {
+
+    public static void main(final String[] args) {
+
+        double piA;
+        double piB;
+        
+
+        piA = 4 * (1.0-0.3+0.2-0.14+0.11-0.09); // JA: You should let the computer do the calculations
+        piB = 4 * (1.0-0.3+0.2-0.14+0.11-0.09+0.07);
+
+        System.out.println(piA);
+        System.out.println(piB);
+
+
+    }
+
+}
