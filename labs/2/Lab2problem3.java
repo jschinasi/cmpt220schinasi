d55839dade71dc02d2cfdd6732c34595585b1fd8
jschinasi <jake.schinasi1@marist.edu>
2020-02-12 19:42:42 -0500

    Lab 2 Problem 3

---
+++ labs/2/Lab2problem3.java
@@ -0,0 +1,28 @@
+// Jake Schinasi Lab2 problem3
+import java.util.Scanner;
+
+public class problem3 {
+    public static void main(String[] args) {
+        // Create scanner to get input
+        Scanner input = new Scanner(System.in);
+        System.out.print( "Enter a low limit integer: " );
+
+        int lowerbound = input.nextInt();
+
+        System.out.print ("Enter an upper bound :"); 
+
+        int upperbound = input.nextInt();
+
+        int random = (int)(Math.random() * (upperbound - lowerbound )) + lowerbound;
+        int random2 = (int)(Math.random() * (upperbound - lowerbound )) + lowerbound;
+        int random3 = (int)(Math.random() * (upperbound - lowerbound )) + lowerbound;
+
+        // Outputs the three random numbers between the upperbound and lowerbound
+        System.out.println("Three random numbers between " + upperbound + " and " 
+        + lowerbound + ":" + "  " + random + "  " + random2 + "  " + random3 );
+
+        
+    }
+
+}
+
