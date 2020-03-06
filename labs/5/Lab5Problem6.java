32a0a0a904fbc61243019628ba1f54aaefaa42ff
jschinasi <jake.schinasi1@marist.edu>
2020-03-06 16:57:02 -0500

    Lab 5 Problem 6

---
+++ labs/5/Lab5Problem6.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+/** Jake Schinasi
+ * Lab5Problem6
+ */
+public class Lab5Problem6 {
+
+    public static void main(final String[] args) {
+        final Scanner input = new Scanner(System.in);
+        double mean = 0;
+        System.out.println("Enter the Number of Rows: ");
+        final int rows = input.nextInt();
+
+        System.out.println("Enter the Number of Columns: ");
+        final int columns = input.nextInt();
+
+        final double[][] matrix = new double[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.println("Enter a Number: ");
+                final int num = input.nextInt();
+                matrix[i][j] = num;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (j < columns - 1)
+                    System.out.print(matrix[i][j] + " ");
+                else
+                    System.out.println(matrix[i][j]);
+            }
+        }
+
+        System.out.println("Enter the Column You Want to Average: ");
+        final int colNum = input.nextInt();
+        mean = averageRow(matrix, colNum - 1);
+        System.out.println("The Average is: " + mean);
+
+    }
+
+    public static double averageRow(final double[][] array, final int row) {
+		double total = 0;
+		double count = 0;
+		double avg = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			total = total + array[i][row];
+			count++;
+		}
+		
+		avg = total/count;
+		return avg;
+    }
+}
